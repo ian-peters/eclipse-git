31ad51f47a72b32da1814d7ecd75b0f33a1dbd8a
Iain <pete0418@algonquinlive.com>
2022-11-04 17:57:09 -0400

    Modified HelloWorld.java to output "Hello World", added comments
    Signed-off-by: Iain <pete0418@algonquinlive.com>

--- Helloworld/src/HelloWorld.java
+++ Helloworld/src/HelloWorld.java
@@ -1,8 +1,15 @@
+/* Name: Iain Peters
+ * Instructor: Stanley Pieda
+ */
 
 public class HelloWorld {
 
+	
+	/*
+	 * Main Method that contains Hello world
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world with Git"); //Hello world ouput
 
 	}
 
