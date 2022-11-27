6683d6d5f4185e80284328bf1e677508510908d4
Iain <pete0418@algonquinlive.com>
2022-11-26 20:30:52 -0500

    Added programmer comments, starter code in main, added my name.
    Signed-off-by: Iain <pete0418@algonquinlive.com>

--- Assignment 3/src/Assignment03IainPeters.java
+++ Assignment 3/src/Assignment03IainPeters.java
@@ -1,8 +1,37 @@
+/*
+ * Student Name: Iain Peters
+ * Lab Professor: Stanley Pieda
+ * Due Date: December 9th, 2022
+ * Last Modified: November 26th, 2022
+ * Description: Assignment 03 starter code. Comments added and updated to output my name.
+ */
 
+/*
+ * Assignment03IainPeters class that contains the main method and two for loops
+ */
 public class Assignment03IainPeters {
 
+	/*
+	 * The main method contains all variables, references to objects, and input/outputs
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int roll = 0;
+		int[] diceRolls = new int[10];
+		int totalRolls = 0;
+
+		// sample the rolls
+		for(int count = 0; count < 1000; count++) {
+		    roll = (int)(Math.random() * 10) + 1;
+		    diceRolls[roll - 1] = diceRolls[roll - 1] + 1; // adjust 1-10 value to 0-9 for index
+		}	
+		// run a report
+		for(int index = 0; index <= diceRolls.length; index++) {  // crashes
+		    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+		    totalRolls = totalRolls + diceRolls[index];
+		}
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Program by Iain Peters");
+
 
 	}
 
